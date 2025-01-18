7bde1cea6fd2ead99dbe43d99626211c2d16f349
Kojima Kohei <125636684+k-kojima-yumemi@users.noreply.github.com>
2025-01-18 17:33:43 +0900

    Build the file

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,26 @@
 import honox from "honox/vite";
+import client from "honox/vite/client";
 import { defineConfig } from "vite";
 
-export default defineConfig({
-    plugins: [
-        honox({
-            client: {
-                input: ["./app/style.css"],
+export default defineConfig(({ mode }) => {
+    if (mode === "client") {
+        return {
+            plugins: [
+                client({
+                    input: ["./app/style.css"],
+                }),
+            ],
+        };
+    }
+
+    return {
+        build: {
+            emptyOutDir: false,
+            ssr: true,
+            rollupOptions: {
+                input: "./app/server.ts",
             },
-        }),
-    ],
+        },
+        plugins: [honox({})],
+    };
 });
