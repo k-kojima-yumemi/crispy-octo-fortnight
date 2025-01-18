6dbf49b6c36fef4ae3ccd23f98840d33aa268bf6
Kojima Kohei <125636684+k-kojima-yumemi@users.noreply.github.com>
2025-01-18 17:08:53 +0900

    Base web server

---
+++ app/routes/index.tsx
@@ -0,0 +1,12 @@
+import { createRoute } from "honox/factory";
+
+export default createRoute((c) => {
+    return c.render(
+        <div>
+            <h1>Hello!</h1>
+        </div>,
+        {
+            title: "Home",
+        },
+    );
+});
--- app/server.ts
+++ app/server.ts
@@ -1,1 +1,8 @@
-console.log("Hello, world!");
+import { showRoutes } from "hono/dev";
+import { createApp } from "honox/server";
+
+const app = createApp();
+
+showRoutes(app);
+
+export default app;
